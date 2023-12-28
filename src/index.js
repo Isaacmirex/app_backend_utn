f475ab66f819686832f6710cd5fb49c9ef462a22
Mario Salazar <mariosalazar.ms.10@gmail.com>
2023-12-28 17:16:01 -0500

    Udate Index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import {router} from './routes/classroom.routes.js';
 import {router_users} from './routes/users.routes.js';
+import {router_auditing} from './routes/auditing.routes.js'
 import {swaggerDocs as V1SwaggerDocs} from './routes/swagger.js';
-import passport from 'passport';
-import './middlewares/microsoft.js';
-import {router_microsoft} from './routes/microsoftlogin.routes.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,16 +10,12 @@ const app = express()
 //middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(passport.initialize())
-
 //swagger
 V1SwaggerDocs(app, PORT);
 //routes
 app.use("/", router);
 app.use("/", router_users);
-app.use('/auth', router_microsoft)
-
-
+app.use("/", router_auditing);
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`);
 });
