678c4dec5b82d79ffe0be1ca79596bbf0b86e6eb
Mario Salazar <mariosalazar.ms.10@gmail.com>
2023-12-29 19:42:18 -0500

    Updates Changes on Login with Microsoft - Token Cookies

--- src/middlewares/microsoft.js
+++ src/middlewares/microsoft.js
@@ -1,8 +1,8 @@
-import passport from "passport";   
-import { Strategy as MicrosoftStrategy } from "passport-microsoft";
-import { config } from "dotenv";
-import { client } from '../database/database.js';
-import { getUserDesByID } from "../controllers/usersController.js" 
+import passport from "passport";
+import {Strategy as MicrosoftStrategy} from "passport-microsoft";
+import {config} from "dotenv";
+import {client} from '../database/database.js';
+import {getUserDesByID} from "../controllers/usersController.js"
 
 config()
 
@@ -17,48 +17,48 @@ passport.serializeUser(function (user, done) {
 
 passport.deserializeUser(async function (id, done) {
     try {
-      const user = await getUserDesByID(id);
+        const user = await getUserDesByID(id);
 
-      if (!user) {
-        console.error('Usuario no encontrado');
-        return done(null, false);
-      }
-      console.log('usuario es: ', user.user_full_name);
+        if (!user) {
+            console.error('Usuario no encontrado');
+            return done(null, false);
+        }
+        console.log('usuario es: ', user.user_full_name);
 
-      // Obtener asignaciones de roles y módulos
-      const assignmentsQueryResult = 
+        // Obtener asignaciones de roles y módulos
+        const assignmentsQueryResult =
             await client.query(`
                 SELECT am.assignment_id, r.rol_name, u.user_full_name , m.module_name
                 FROM assignments_modules AS am
                 INNER JOIN roles AS r ON am.rol_id = r.rol_id
                 INNER JOIN modules AS m ON am.module_id = m.module_id
                 INNER JOIN users AS u ON am.user_id = u.user_id WHERE u.user_id = $1`, [user.user_id]);
-      const roles = assignmentsQueryResult.rows.map(row => row.rol_name);
-      console.log('roles:', roles);
-      const modules = assignmentsQueryResult.rows.map(row => row.module_name);
-      console.log('modulos:', modules);
-      // Pasar roles y módulos al completar la deserialización
-      done(null, { user, roles, modules });
+        const roles = assignmentsQueryResult.rows.map(row => row.rol_name);
+        console.log('roles:', roles);
+        const modules = assignmentsQueryResult.rows.map(row => row.module_name);
+        console.log('modulos:', modules);
+        // Pasar roles y módulos al completar la deserialización
+        done(null, {user, roles, modules});
     } catch (error) {
-      done(error, null);
+        done(error, null);
     }
 });
 
-  
+
 
 passport.use(
     "auth-microsoft",
     new MicrosoftStrategy({
         clientID: process.env.MICROSOFT_CLIENT_ID,
         clientSecret: process.env.MICROSOFT_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/microsoft/callback",
+        callbackURL: "https://app-backend-utn-2023.onrender.com/auth/microsoft/callback",
         scope: ["user.read", "calendars.read", "mail.read", "offline_access"],
         authorizationURL: 'https://login.microsoftonline.com/8dbe1469-c79c-4e21-9d43-ca65d9e9c475/oauth2/v2.0/authorize',
         tokenURL: 'https://login.microsoftonline.com/8dbe1469-c79c-4e21-9d43-ca65d9e9c475/oauth2/v2.0/token',
     }, async function (req, accessToken, refreshToken, profile, done) {
         try {
-
-            console.log('token:', accessToken);
+            done(null, {profile, accessToken, refreshToken})
+            //console.log('token:', accessToken);
             // Consultar el usuario por su user_code
             const userQueryResult = await client.query('SELECT * FROM users WHERE user_code = $1', [profile.id]);
             let userFromDB;
@@ -87,7 +87,7 @@ passport.use(
                     ON CONFLICT (user_code) DO NOTHING
                     RETURNING *;
                 `, [
-                    userData.user_code, userData.user_full_name, userData.user_first_name, 
+                    userData.user_code, userData.user_full_name, userData.user_first_name,
                     userData.user_last_name, userData.user_email, userData.user_phone_number,
                     userData.user_state, userData.user_date_register
                 ]);
@@ -102,7 +102,7 @@ passport.use(
               req.session.roles = assignmentsQueryResult.rows.map(row => row.rol_id);
               req.session.modules = assignmentsQueryResult.rows.map(row => row.module_id);
             }*/
-        
+
             // Llamar a la función done para indicar que la autenticación fue exitosa
             done(null, profile);
 
@@ -111,18 +111,18 @@ passport.use(
             done(error, null);
         }
     }
-));
-
-    
+    ));
 
 
 
 
 
-  
-  
 
 
 
-  
+
+
+
+
+
 
--- src/routes/microsoft.js
+++ src/routes/microsoft.js
@@ -1,31 +1,47 @@
-import { Router } from "express";
+import {Router} from "express";
 import passport from "passport";
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
 
 const loginRouter = Router();
 
+loginRouter.use(cookieParser());
+loginRouter.use(passport.initialize());
 
-loginRouter.get(
-    "/microsoft", 
-    passport.authenticate("auth-microsoft", {
-        prompt: "select_account",
-        session: true,
-    })
-);
 
-loginRouter.get(
-    "/microsoft/callback",
-    passport.authenticate("auth-microsoft", {
-        failureRedirect: "/auth/microsoft",
-        session: true,
-    }),
-    (req, res) => {
-        
-       //res.json(req.user);
-       res.redirect('/utnbackend/v1/users')
+loginRouter.get('/microsoft', (req, res, next) => {
+    passport.authenticate('auth-microsoft', {prompt: 'select_account', session: false}, (err, user, info) => {
+        console.log("Mario Salazar: " + user)
+        if (err || !user) {
+            return res.status(401).json({message: 'Authentication failed'});
+        }
+        res.json({token});
+    })(req, res, next);
+});
 
+loginRouter.get('/microsoft/callback', passport.authenticate("auth-microsoft", {
+    failureRedirect: '/auth/microsoft',
+    session: false
+}), (req, res) => {
+    try {
+        const token = req.user.refreshToken.access_token;
+        console.log(token)
+        const userString = JSON.stringify(token)
+        res.send(`
+        <!DOCTYPE html>
+    <html lang="en">
+      <body>
+      </body>
+      <script>
+        window.opener.postMessage(${userString}, 'http://localhost:5173')
+      </script>
+    </html>
+        `)
+    } catch (error) {
+        res.json({error: error})
     }
+});
 
-);
 
-
-export { loginRouter };
+export {loginRouter};
